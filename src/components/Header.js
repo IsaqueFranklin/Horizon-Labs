9969a28b3ec69d1a7abfda62df280268d959b5f1
ISAQUE FRANKLIN AMARAL <isaquefrankli@gmail.com>
2023-03-27 19:36:04 -0300

    blog

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                     {/*<Nav.Link style={{marginLeft: 20}} href="/about">Sobre</Nav.Link>*/}
-                    <Nav.Link className="header" href="/"><button>Blog</button></Nav.Link>
+                    <Nav.Link className="header" href="/"><button>Portal</button></Nav.Link>
                     {/*<Nav.Link style={{marginLeft: 20, color: '#fff'}} href="/planos">Planos</Nav.Link>
                     <Nav.Link style={{marginLeft: 20, color: '#fff'}} href="/contato">Contato</Nav.Link>*/}
                     {/*{user && (
